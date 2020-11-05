454461bddea4992849c279f3766d82827737bc08
Walter Doekes <walter+github@wjd.nu>
2020-11-05 14:53:30 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='voiputil',
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['monami', 'monamish'],
     entry_points='''
         [console_scripts]
